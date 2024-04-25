063357a812c8a09732cfa50fbf5cf0d399619753
Fernando Santiago <105754273+fsaantiago@users.noreply.github.com>
2024-04-25 20:30:09 -0300

    Add files via upload

---
+++ fernando-santiago/minha-prova/av1-arq/src/main/java/com/ibge/av1arq/entity/Release.java
@@ -0,0 +1,64 @@
+package com.ibge.av1arq.entity;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document (collection = "releases")
+public class Release   {
+
+        @Id
+        private String id;
+        private String titulo;
+        private String conteudo;
+        private String data;
+        private String tipo;
+
+    public Release(String id, String titulo, String conteudo, String data, String tipo) {
+        this.id = id;
+        this.titulo = titulo;
+        this.conteudo = conteudo;
+        this.data = data;
+        this.tipo = tipo;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getConteudo() {
+        return conteudo;
+    }
+
+    public void setConteudo(String conteudo) {
+        this.conteudo = conteudo;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
